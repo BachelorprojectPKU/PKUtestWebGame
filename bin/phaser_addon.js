9a73412ba29068d8b341b79b087f4942cc814cd6
BdR76 <bdr1976@gmail.com>
2020-05-16 15:03:28 +0200

    Move function moveScene to scene level
    Move function moveScene to scene level

--- bin/phaser_addon.js
+++ bin/phaser_addon.js
@@ -46,8 +46,54 @@ Phaser.Scene.prototype.addButtonText = function(x, y, key, callback, callbackCon
 		btn.on('pointerdown', function (ptr)       { this.setScale(0.96, 0.96) } );
 		//btn.on('pointerup', callback.bind(callbackContext));
 		btn.on('pointerup',   function (ptr, x, y) {
+			debugger;
 			this.ondowncallback.call(this.ctx, this);
 			} );
 		
 		return btn;
+};
+
+// move container in a scene
+// navigate between different GUI parts
+Phaser.Scene.prototype.moveScene = function(cntScene, iMoveCode, xOffset)
+{
+	debugger;
+
+	// xOffset is optional
+	if (typeof xOffset === "undefined") xOffset = 0;
+	// set move values
+	var width = this.game.config.width;
+	var xStart = -1 * width;
+	var xGoal = 0;
+	//if (iMoveCode == MENU_ENTER_LEFT)  {xStart = -1 * this.width; xGoal =    0};
+	if (iMoveCode == MENU_ENTER_RIGHT) {xStart = width;  xGoal = 0};
+	if (iMoveCode == MENU_EXIT_LEFT)   {xStart = 0;      xGoal = -1* width};
+	if (iMoveCode == MENU_EXIT_RIGHT)  {xStart = 0;      xGoal = width};
+	// optionally correct for offset
+	var xStart = xStart + xOffset;
+	var xGoal = xGoal + xOffset;
+
+	// set tween animation values
+	cntScene.visible = true;
+	cntScene.x = xStart;
+	
+	// slide container with menu text and buttons
+	var tw = this.tweens.add(
+		{
+			targets: cntScene,
+			x: xGoal,
+			ease: "Power3",
+			duration: 400, // duration of animation; higher=slower
+			onComplete: this.onMoveSceneComplete.bind(this)
+		}
+	);
+};
+
+Phaser.Scene.prototype.onMoveSceneComplete = function(tw, ary)
+{
+	// make container invisible
+
+	// not visible if container is exit to left (x = -480) or to right (x = 480)
+	var cnt = ary[0];
+	cnt.visible = (cnt.x == 0);
 };
--- js/pkututor2.js
+++ js/pkututor2.js
@@ -227,46 +227,6 @@ var Tutorial2 = new Phaser.Class({
         console.log("tutorial 2 doStart was called!");
 		
 		this.scene.start("pkugame2");
-    },
-	
-	moveScene: function(cntScene, iMoveCode, xOffset) {
-		// xOffset is optional
-		if (typeof xOffset === "undefined") xOffset = 0;
-		// set move values
-		var width = this.game.config.width;
-		var xStart = -1 * width;
-		var xGoal = 0;
-		//if (iMoveCode == MENU_ENTER_LEFT)  {xStart = -1 * this.width; xGoal =    0};
-		if (iMoveCode == MENU_ENTER_RIGHT) {xStart = width;  xGoal = 0};
-		if (iMoveCode == MENU_EXIT_LEFT)   {xStart = 0;      xGoal = -1* width};
-		if (iMoveCode == MENU_EXIT_RIGHT)  {xStart = 0;      xGoal = width};
-		// optionally correct for offset
-		var xStart = xStart + xOffset;
-		var xGoal = xGoal + xOffset;
-
-		// set tween animation values
-		cntScene.visible = true;
-		cntScene.x = xStart;
-		
-		// slide container with menu text and buttons
-		var tw = this.tweens.add(
-			{
-				targets: cntScene,
-				x: xGoal,
-				ease: "Power3",
-				duration: 400, // duration of animation; higher=slower
-				onComplete: this.onMoveSceneComplete.bind(this)
-			}
-		);
-	},
-
-	onMoveSceneComplete: function(tw, ary) {
-		// make container invisible
-
-		// not visible if container is exit to left (x = -480) or to right (x = 480)
-		var cnt = ary[0];
-		cnt.visible = (cnt.x == 0);
-	},
-
+    }
 
 });
